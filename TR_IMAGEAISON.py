fa29d533cdb120e458fc35f1ab37e6794ab84d91
T-Sana <tim.tamet@gmail.com>
2023-07-01 22:16:33 +0200

    Q

--- TR_IMAGEAISON.py
+++ TR_IMAGEAISON.py
@@ -52,18 +52,22 @@ if True: ## Img2 ## @@ Ville @@
             tla, tlb, tlc, tld, tle, tlf = tailles
             dtt = 100
         if True: ## Mur.vars ##
-            mur = [pt_sg(hg, bg, 9, 5), pt_sg(hd, bd, 4, 9), 5]
-            crenaux = [pt_sg(hg, bg, 9, 4), pt_sg(hd, bd, 9, 5), 19] ## Merlets [CATALÀ] ##
+            mur = [pt_sg(hg, bg, 9, 3), pt_sg(hd, bd, 9, 5), 5]
+            crenaux = [pt_sg(hg, bg, 9, 2), pt_sg(hd, bd, 9, 3), 19] ## Merlets [CATALÀ] ##
+            distance = diff(crenaux[0][0], crenaux[1][0])/crenaux[2]
         if True: ## Porte.vars ##
             porte = 4
             d_porte = 12
         if True: ## VARS ##
             d = [haut*2, long, 3]
-            hg, hd, bg, bd = [0, 0], [d[1], 0], [0, d[0]], [d[1], d[0]]
-            ct = ct_cr(hg, hd, bg, bd)
     def img2():
-        c_porte = []
-        img = image(remplissage=nouvelle_couleur('e08030'), dimensions=d)
+        if True: ## VARS ##
+            hg = [0, 0]
+            bg = [0, haut]
+            hd = [long, 0]
+            bd = [long, haut]
+            c_porte = []
+            img = image(remplissage=nouvelle_couleur('e08030'), dimensions=d)
         soleil(img, [hd[0]-dtt, hd[1]+dtt])
         if True: ## Nuages ##
             nuage(img, [cth[0]+200, cth[1]+75], tld, [220, 220, 220])
@@ -75,9 +79,8 @@ if True: ## Img2 ## @@ Ville @@
         if True: ## Sol ##
             rectangle(img, [0, d[0]], pt_sg(hd, bd, 9, 4), nouvelle_couleur('80e030'), 0) ## Terre (rectangle vert) ##
         if True: ## Mur ##
-            
             dy = diff(mur[0][1], mur[1][1])/mur[2]
-            dx = diff(mur[0][0], mur[1][0])/crenaux[2]
+            dx = diff(mur[0][0], mur[1][0])/crenaux[2]/mur[2]
             truc = False
             for a, j in enumerate(range2(mur[0][1], mur[1][1], dy)):
                 for b, i in enumerate(range2(mur[0][0] - (dx//2 if truc else 0), mur[1][0] + (dx//2 if truc else 0), dx)):
@@ -108,7 +111,7 @@ if True: ## Img2 ## @@ Ville @@
                     if t or a == 0 or a == 1:
                         rectangle(img, [i, j], [i+dx, j+dy], nouvelle_couleur('404040'), 0)
                         rectangle(img, [i, j], [i+dx, j+dy], noir, 3)
-                    if diff(j, mur[1][1]) < dy:
+                    if diff(j, mur[1][1]) <= dy:
                         if b == d_porte+porte:
                             if truc: dx2 = dx/2
                             else: dx2 = 0
@@ -119,13 +122,18 @@ if True: ## Img2 ## @@ Ville @@
             for i in c_porte:
                 point(img, i)
         if True: ## Crénaux ##
-            distance = diff(crenaux[0][0], crenaux[1][0])/crenaux[2]
             dra = True
-            for i in range2(crenaux[0][0]-distance/2, crenaux[1][0]+distance/2, distance):
-                if dra:
-                    rectangle(img, [i, crenaux[0][1]], [i+distance, crenaux[1][1]], nouvelle_couleur('404040'), 0)
-                    rectangle(img, [i, crenaux[0][1]], [i+distance, crenaux[1][1]], noir, 3)
-                dra = not dra
+            chng = False
+            for i in range2(crenaux[0][0]-distance/2, crenaux[1][0]+distance/2, dx):
+                if dra and not chng:
+                    rectangle(img, [i, crenaux[1][1]-dy], [i+distance/8*3, crenaux[1][1]], nouvelle_couleur('404040'), 0)
+                    rectangle(img, [i, crenaux[1][1]-dy], [i+distance/8*3, crenaux[1][1]], noir, 3)
+                if chng:
+                    dra = not dra
+                chng = not chng
+        if True: ## VARS update ##
+            hg, hd, bg, bd = [0, 0], [d[1], 0], [0, d[0]], [d[1], d[0]]
+            ct = ct_cr(hg, hd, bg, bd)
         return(img)
 
 def img_cart1() -> None:
@@ -143,3 +151,4 @@ def demo_img2():
         wk = montre_part(im2, pto=[0, i], attente=1, destroy=non)
         if wk == 27: quitter()
     montre_part(im2)
+demo_img2()
--- outils/functs.py
+++ outils/functs.py
@@ -1,12 +1,15 @@
 from multimethod import multimethod
 @multimethod
-def range2(start, stop, step: int=1):
+def range2(start: int, stop: int, step: int=1):
+    start = round(start)
+    stop = round(stop)
+    step = round(step)
     return(range(start, stop, step))
 @multimethod
-def range2(stop):
+def range2(stop: int):
     return(range(stop))
 @multimethod
-def range2(start, stop, step: float):
+def range2(start, stop, step):
     if step == 0: return([])
     out = [start]
     a = start
